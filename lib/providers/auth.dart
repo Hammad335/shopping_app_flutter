936ca90589d7a06ddc08dfa150b02bb29f28e079
Hamad <hamadmemon946@gmail.com>
2022-06-01 03:38:44 +0500

    added auth screen UI

---
+++ lib/providers/auth.dart
@@ -0,0 +1,9 @@
+// import 'package:flutter/material.dart';
+//
+// class Auth with ChangeNotifier {
+//   String _token;
+//   DateTime _tokenExpiryDate;
+//   String _userId;
+//
+//   Future<void> signup(String email, String password) async {}
+// }
